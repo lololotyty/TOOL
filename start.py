ab0b481b1c7db5f2c97dfa26148918672df191f6
Rohan Roy <mdabdurrahman.6700@gmail.com>
2024-06-04 14:53:57 +0530

    Add files via upload

---
+++ start.py
@@ -0,0 +1,25 @@
+import os
+import sys
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+
+from info import Config, Txt
+
+
+@Client.on_message(filters.private & filters.command('start'))
+async def handle_start(bot:Client, message:Message):
+
+    Btn = [
+        [InlineKeyboardButton(text='🌷 Hᴇʟᴘ 🌷', callback_data='help'), InlineKeyboardButton(text='🌷 Sᴇʀᴠᴇʀ Sᴛᴀᴛs 🌷', callback_data='server')],
+        [InlineKeyboardButton(text='🌷 Uᴘᴅᴀᴛᴇs 🌷', url='https://t.me/ilovemovie143'), InlineKeyboardButton(text='🌷 Aʙᴏᴜᴛ 🌷', callback_data='about')],
+        [InlineKeyboardButton(text='🌷 Dᴇᴠᴇʟᴏᴘᴇʀ 🌷', url='https://t.me/itz_me_rohu')]
+        ]
+
+    await message.reply_text(text=Txt.START_MSG.format(message.from_user.mention), reply_markup=InlineKeyboardMarkup(Btn))
+
+
+#Restart to cancell all process 
+@Client.on_message(filters.private & filters.command("restart") & filters.user(Config.SUDO))
+async def restart_bot(b, m):
+    await m.reply_text("💥__Rᴇꜱᴛᴀʀᴛɪɴɢ.....__")
+    os.execl(sys.executable, sys.executable, *sys.argv)
