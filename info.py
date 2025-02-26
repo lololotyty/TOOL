57a801cb8fdcecd1372dcf8aef5eec7ad5efefea
wddwwdw <hangerskype@gmail.com>
2025-02-27 01:31:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,28 +1,20 @@
 import os
 
 class Config:
-    # Bot token from @BotFather
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "7680113655:AAGBrFFksT0yDDfoHEci3B42WM8h31Su3c4")
-    
-    # API ID & Hash from my.telegram.org
-    API_ID = os.environ.get("API_ID", "22430866")
+    API_ID = int(os.environ.get("API_ID", "22430866"))  # Convert to int
     API_HASH = os.environ.get("API_HASH", "20a0f0284c49fe38bb3676c297fbf947")
-    
-    # Sudo users (Owner users)
     SUDO = [int(x) for x in os.environ.get("SUDO", "6169016546").split()]
-    
-    # Start message pic
     START_PIC = os.environ.get("START_PIC", "https://te.legra.ph/file/29626078a1324cf58ce2a.jpg")
-    
-    # Support chat link
     SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "ilovemovie123")
+    OWNER = int(os.environ.get("OWNER", "6169016546"))  # Add owner ID
 
 class Txt:
-    # Messages
     START_MSG = """
-Hi {}, Welcome to Mass Report Bot.
+👋 Hi {}, Welcome to Mass Report Bot!
 
 I can help you mass report Telegram channels/groups.
+Use /help to see available commands.
     """
     
     HELP_MSG = """
